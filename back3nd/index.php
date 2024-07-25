6d088560051f492db8e288b3beb5d4027029cb50
andijayawizard <andijayawizard@gmail.com>
2024-07-25 16:01:32 +0700

    update sore

--- back3nd/index.php
+++ back3nd/index.php
@@ -1,5 +1,10 @@
 <?php
 // require_once 'load.php';
+use DevCoder\DotEnv;
+require_once 'src/Controllers/DotEnv.php';
+$dotEnv=new DotEnv('.env');
+$dotEnv->load();
+
 if (!empty($_SESSION)) {
 } else {
   session_start();
@@ -7,15 +12,65 @@ if (!empty($_SESSION)) {
 ?>
 <!doctype html>
 <html lang="en" data-bs-theme="dark">
-<?php 
-include "head.php"; 
-require_once 'layout-login.php';
-?>
-<?php 
-// if (isset($_SESSION['ADMIN'])) { 
-//   require_once 'layout-admin.php';
-// } else {
-//   require_once 'layout-login.php';
-// } ?>
+  <?php 
+  include "head.php"; 
+  // require_once 'layout-login.php';
+  ?>
+  <body class="hold-transition login-page dark-mode-">
+    <div class="login-box">
+      <div id="logout">
+        <?php if (isset($_GET['signout'])) { ?>
+        <div class="alert alert-success">
+          <small>You've been logged out!</small>
+        </div>
+        <?php } ?>
+      </div>
+      <div id="notifikasi">
+        <div class="alert alert-danger">
+          <small>Login Anda Gagal, Periksa Kembali Username dan Password</small>
+        </div>
+      </div>
+
+      <div class="login-logo">
+        <!-- <a href="#"><?= $misc['nama']; ?></a> -->
+        <!-- <a href="#"><b>Waterproofing</b>Padang</a> -->
+      </div>
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
+          <form method="post" action="<?=getenv('baseUrl')?>/back3nd/proses.php?aksi=login" id="formLogin">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Email" name="username">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="password" class="form-control" placeholder="Password" name="password">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-8">
+              </div>
+              <!-- /.col -->
+              <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+              </div>
+              <!-- /.col -->
+            </div>
+          </form>
+          <!-- /.social-auth-links -->
+        </div>
+        <!-- /.login-card-body -->
+      </div>
+    </div>
+  </body>
 </html>
 
